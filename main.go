e3948f1fe0cb9b1daec038d96cf2d5d856ff4da4
jibola <pro.ajibolaojo@gmail.com>
2023-09-07 01:03:24 +0100

    complete

--- main.go
+++ main.go
@@ -21,22 +21,33 @@ type Message struct {
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-	m := Message{
-		Slack_name:      "jibs",
-		Current_day:     now.Weekday().String(),
-		Utc_time:        now.UTC().String(),
-		Track:           "backend",
-		Github_file_url: "https://github.com/jbrit/goone/main.go",
-		Github_repo_url: "https://github.com/jbrit/goone",
-		Status_code:     200,
+	w.Header().Set("Content-Type", "application/json")
+	// validate request
+	if !r.URL.Query().Has("slack_name") {
+		w.WriteHeader(400)
+		io.WriteString(w, "{\"error\": \"include 'slack_name' in the query string\"}")
+	} else if !r.URL.Query().Has("track") {
+		w.WriteHeader(400)
+		io.WriteString(w, "{\"error\": \"include 'track' in the query string\"}")
+	} else {
+
+		now := time.Now()
+		m := Message{
+			Slack_name:      r.URL.Query().Get("slack_name"),
+			Current_day:     now.Weekday().String(),
+			Utc_time:        now.UTC().Format(time.RFC3339),
+			Track:           r.URL.Query().Get("track"),
+			Github_file_url: "https://github.com/jbrit/goone/main.go",
+			Github_repo_url: "https://github.com/jbrit/goone",
+			Status_code:     200,
+		}
+		b, err := json.MarshalIndent(m, "", "  ")
+		if err != nil {
+			fmt.Printf("error marshalling json: %s\n", err)
+		}
+		fmt.Printf("got / request\n")
+		io.WriteString(w, string(b))
 	}
-	b, err := json.MarshalIndent(m, "", "  ")
-	if err != nil {
-		fmt.Printf("error marshalling json: %s\n", err)
-	}
-	fmt.Printf("got / request\n")
-	io.WriteString(w, string(b))
 }
 
 func main() {
